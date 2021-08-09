8c18972dc35b2d78786a53b6bcb6ccd5ba0d9442
Ben Kaminski <ben@benkaminski.com>
2021-08-09 01:38:26 -0400

    finally figured out gulp integration

--- functions/theme-dependencies.php
+++ functions/theme-dependencies.php
@@ -1,18 +1,14 @@
 <?php 
 //LOAD SCRIPTS
 function enqueue_dmi_scripts() {
-    //wp_enqueue_script('Bootstrap-5.0', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js', array('jquery'), '', true);
-    //wp_enqueue_script('font-awesome-5', 'https://kit.fontawesome.com/42219cbebc.js', false, null, null, false);
     wp_enqueue_script('dmi-scripts', get_template_directory_uri() . '/assets/js/webflow.js' , array(), false, true );
 }
 add_action('wp_enqueue_scripts', 'enqueue_dmi_scripts');
 
+
 //LOAD CSS
 function enqueue_dmi_styles() {
-    //wp_enqueue_style('bootstrap-5.0', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css');
-    wp_enqueue_style('webflow', get_template_directory_uri() . '/assets/styles/webflow.css');
-    wp_enqueue_style('normalize', get_template_directory_uri() . '/assets/styles/normalize.css');
-    wp_enqueue_style('dmi-styles', get_template_directory_uri() . '/assets/styles/diamond-state-logistics.webflow.css');
+    wp_enqueue_style('dmi-styles', get_template_directory_uri() . '/assets/styles/dmiStyles.min.css');
    
 }
 add_action('wp_enqueue_scripts', 'enqueue_dmi_styles');
--- header.php
+++ header.php
@@ -7,10 +7,10 @@
   <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
   <script>WebFont.load({  google: {    families: ["Poppins:regular,italic,600,600italic"]  }});</script>
   <script>
-  !function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);
+      !function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);
   </script>
+  <link href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon.png" rel="shortcut icon" type="image/x-icon">
   <?php wp_head(); ?>
-
 </head>
 <body <?php body_class(); ?>>
-<?php get_template_part( 'parts/page', 'main-nav' ); ?>
+    <?php get_template_part( 'parts/page', 'main-nav' ); ?>
