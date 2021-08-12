ff260f1e5c8ecbeb6064eae5815d3c063d7287ca
Ben Kaminski <ben@benkaminski.com>
2021-08-11 22:23:03 -0400

    placed proper imagery on home page

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
   <?php get_template_part( 'parts/page', 'header-tracking' ); ?>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1" name="viewport">
-  <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
   <script>WebFont.load({  google: {    families: ["Poppins:regular,italic,600,600italic"]  }});</script>
   <script>
       !function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);
--- parts/page-main-nav.php
+++ parts/page-main-nav.php
@@ -1,7 +1,7 @@
 <header>
   <div data-collapse="medium" data-animation="default" data-duration="400" role="banner" class="navbar w-nav">
       <div class="container-1140 mj-nav-container w-container">
-        <nav role="navigation" class="nav w-nav-menu">
+        <nav class="nav w-nav-menu">
           <a href="<?php echo get_home_url(); ?>/about" class="navy-link left-most-nav-link w-nav-link">About</a>
           <a href="<?php echo get_home_url(); ?>/contact" class="navy-link w-nav-link">Contact</a>
           <a href="<?php echo get_home_url(); ?>" aria-current="page" class="w-nav-brand w--current"><img src="<?php echo get_home_url(); ?>/wp-content/uploads/FinalLogoDiamondState.png" loading="lazy" width="192" sizes="(max-width: 991px) 100vw, 192px" srcset="<?php echo get_home_url();?>/wp-content/uploads/FinalLogoDiamondState-p-500.png 500w, wp-content/uploads/FinalLogoDiamondState-p-800.png 800w, wp-content/uploads/FinalLogoDiamondState.png 8031w" alt="" class="image-2"></a>
@@ -18,7 +18,7 @@
       <div data-collapse="medium" data-animation="default" data-duration="400" role="banner" class="mobile-nav w-nav">
         <div class="container-1281 w-container">
           <a href="<?php echo get_home_url(); ?>" class="w-nav-brand"><img src="<?php echo get_home_url();?>/wp-content/uploads/FinalLogoDiamondState.png" loading="lazy" width="102" sizes="(max-width: 991px) 102px, 100vw" srcset="<?php echo get_home_url();?>/wp-content/uploads/FinalLogoDiamondState-p-500.png 500w, <?php echo get_home_url();?>/wp-content/uploads/FinalLogoDiamondState-p-800.png 800w, <?php echo get_home_url();?>/wp-content/uploads/FinalLogoDiamondState.png 8031w" alt="Diamond State Logistics Logo"></a>
-          <nav role="navigation" class="mobile-nav-wrapper w-nav-menu">
+          <nav class="mobile-nav-wrapper w-nav-menu">
             <a href="<?php echo get_home_url(); ?>" class="mobile-nav-link w-nav-link">Home</a>
             <a href="<?php echo get_home_url(); ?>/about" class="mobile-nav-link w-nav-link">About</a>
             <a href="<?php echo get_home_url(); ?>/services" class="mobile-nav-link w-nav-link">Services</a>
