4e37bc091ce2e8e52bb23289d0b6960f99d9ee14
Ben Kaminski <ben@benkaminski.com>
2021-08-11 22:58:08 -0400

    began adding ACF to build

--- parts/home-services.php
+++ parts/home-services.php
@@ -1,25 +1,25 @@
 <main class="services-section section-in-base wf-section">
     <div class="container-1140 services-container">
-      <div data-w-id="f43161e7-6f80-752f-a088-f32f17d8485e" class="serv-heading-div">
-        <h2 data-w-id="1069485e-8d88-3eca-e76e-e269d893aa52" class="service-heading homepage-heading">What We Offer</h2>
+      <div data-w-id="f43161e7-6f80-752f-a088-f32f17d8485e" style="opacity:0" class="serv-heading-div">
+        <h2 data-w-id="1069485e-8d88-3eca-e76e-e269d893aa52" style="opacity:0" class="service-heading homepage-heading"><?php echo the_field('service_title');?></h2>
       </div>
       <div class="services-grid">
         <div id="w-node-_95500e95-ec38-44d7-37ae-206830ef4990-f7e4b13a" data-w-id="95500e95-ec38-44d7-37ae-206830ef4990" class="service-wrapper">
-          <div class="div-block-4 home-service-1"></div>
-          <h4 class="services-heading" style="text-align:center;">CDL Hauling</h4>
-          <div class="text-block-3">Coming soon! In the near future, we will offer power-only CDL Hauling / Transportation service.</div>
+          <div class="div-block-4 home-service-1" style="background-image: url('<?php echo the_field('service_1_image'); ?>');"></div>
+          <h4 class="services-heading" style="text-align:center;"><?php echo the_field('service_1_title');?></h4>
+          <div class="text-block-3"><?php echo the_field('service_1_copy');?></div>
           <a href="<?php echo get_home_url(); ?>/services" class="service-cta homepage-btn">Learn More</a>
         </div>
-        <div data-w-id="d037c604-35c9-a69c-0103-98a46019dbf8" class="service-wrapper">
-          <div class="div-block-4 home-service-2"></div>
-          <h4 class="heading-3">Box-Truck Hauling</h4>
-          <div class="text-block-4">We currently offer Box-Truck Hauling service using a 26 foot box truck with lift gate.</div>
+        <div data-w-id="d037c604-35c9-a69c-0103-98a46019dbf8" style="opacity:0" class="service-wrapper">
+          <div class="div-block-4 home-service-2" style="background-image: url('<?php echo the_field('service_2_image'); ?>');"></div>
+          <h4 class="heading-3"><?php echo the_field('service_2_title');?></h4>
+          <div class="text-block-4"><?php echo the_field('service_2_copy');?></div>
           <a href="<?php echo get_home_url(); ?>/services" class="service-cta homepage-btn">Learn More</a>
         </div>
-        <div data-w-id="37fb3c60-5158-978b-6b39-9a1fec511ca3" class="service-wrapper">
-          <div class="div-block-4 home-service-3"></div>
-          <h4 class="heading-4">Custom Services</h4>
-          <div class="text-block-5">Our Box Truck service offers a variety of pick up and drop off options. </div>
+        <div data-w-id="37fb3c60-5158-978b-6b39-9a1fec511ca3" style="opacity:0" class="service-wrapper">
+          <div class="div-block-4 home-service-3" style="background-image: url('<?php echo the_field('service_3_image'); ?>');"></div>
+          <h4 class="heading-4"><?php echo the_field('service_1_title');?></h4>
+          <div class="text-block-5"><?php echo the_field('service_3_copy');?></div>
           <a href="<?php echo get_home_url(); ?>/services" class="service-cta homepage-btn">Learn More</a>
         </div>
       </div>
